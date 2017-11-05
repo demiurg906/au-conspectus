14ed0ec23c3ec9579ef295e9b07058a641a92c61
xamgore <xamgore@ya.ru>
2017-11-05 11:55:26 +0300

    Change the message template of the telegram bot

--- telegram/message_generator.py
+++ telegram/message_generator.py
@@ -1,32 +1,20 @@
 import sys
 
 
-def prepare_message(s: str):
-    res = ' '.join(s.split()[1:])
-    url = ''
-    if res.endswith('.md'):
-        name = res.split('/')[-1]
-        name = name.replace('.md', '.html')
-        url = 'https://xamgore.github.io/au-conspectus/{}'.format(name)
-    return res, url
-
-
-def print_message(message: str, info: str, removed: bool = False):
-    res, url = prepare_message(message)
-    print('"{}" {}'.format(res, info))
-    if url and not removed:
-        print('take a look: {}'.format(url))
-    print()
+def parse(s: str):
+    path = ' '.join(s.split()[1:])
+    name = path.split('/')[-1][:-3]
+    url = 'https://xamgore.github.io/au-conspectus/{}'.format(name + '.html')
+    return name, url
 
 
 if __name__ == '__main__':
-    ms = sys.stdin.readlines()
-    for message in reversed(ms):
-        if message.startswith('A'):
-            print_message(message, 'added')
-        elif message.startswith('M'):
-            print_message(message, 'modified')
-        elif message.startswith('D'):
-            print_message(message, 'removed', True)
-        else:
-            break
+    lines = [l.strip() for l in sys.stdin.readlines()]
+    lectures = [l for l in reversed(lines) if l[:1] == 'A' and l[-3:] == '.md']
+
+    if len(lectures) == 1:
+        _, url = parse(lectures[0])
+        print(':boom: Новая лекция! [глянуть]({})'.format(url))
+    if len(lectures) > 1:
+        print(':boom: Пачка новых лекций!')
+        print(*(' · [%s](%s)' % parse(l) for l in lectures), sep='\n')
